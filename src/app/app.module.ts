9b2aff41758c90dc3b3b5277a34bda5aacdb4d98
rsorrt <akumpum@gmail.com>
2021-06-05 19:10:18 +0200

    2 files modified, 2 added, 2 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
+import { ProveComponent } from './prove/prove.component';
+
 
 @NgModule({
   imports:      [ 
@@ -14,7 +16,14 @@ import { HelloComponent } from './hello.component';
     HttpClientModule
   ],
   
-  declarations: [ AppComponent, HelloComponent ],
+  
+  declarations: [ 
+    AppComponent,
+    HelloComponent, 
+    ProveComponent
+    ],
+
+
   bootstrap:    [ AppComponent ],
   providers: []
 })
---
+++ src/app/prove/prove.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-prove',
+  templateUrl: './prove.component.html'
+  
+})
+export class ProveComponent implements OnInit {
+  
+  nuovoTodo : string = 'ciao';
+  
+  
+  
+  constructor() {}
+ 
+  ngOnInit() {
+
+  }
+
+  
+
+
+
+
+}
